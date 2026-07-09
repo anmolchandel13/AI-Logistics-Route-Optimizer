631de0ccf64a0478c8f07b2ae2be3f66ea679901
anmolchandel13 <anmolchandel13@gmail.com>
2026-07-09 21:34:54 +0530

    Upload backend/src/main/java/com/logistics/optimizer/dto/request/UpdateProfileRequest.java

---
+++ backend/src/main/java/com/logistics/optimizer/dto/request/UpdateProfileRequest.java
@@ -0,0 +1,19 @@
+package com.logistics.optimizer.dto.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequest {
+
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    private String name;
+
+    private String phone;
+
+    private String company;
+}
