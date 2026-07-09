78ac972af98e6c409079834c25fe27da0b151485
anmolchandel13 <anmolchandel13@gmail.com>
2026-07-09 21:34:33 +0530

    Upload backend/src/main/java/com/logistics/optimizer/LogisticsOptimizerApplication.java

---
+++ backend/src/main/java/com/logistics/optimizer/LogisticsOptimizerApplication.java
@@ -0,0 +1,12 @@
+package com.logistics.optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsOptimizerApplication.class, args);
+    }
+}
