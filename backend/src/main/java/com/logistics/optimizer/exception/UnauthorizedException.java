4279f4611e11a90c10dc17080515722db2c37238
anmolchandel13 <anmolchandel13@gmail.com>
2026-07-09 21:35:27 +0530

    Upload backend/src/main/java/com/logistics/optimizer/exception/UnauthorizedException.java

---
+++ backend/src/main/java/com/logistics/optimizer/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.logistics.optimizer.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
